351f2114fc728136e19393039ac44f7128604289
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-31 12:56:09 +0000

    up

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function calculate() {
+  let inputedAge = document.getElementById("age").value
+
+  if (inputedAge >= 17) {
+    document.getElementById("answer").innerHTML = "you can see an R movie alone"
+  } else if (inputedAge >= 13) {
+    document.getElementById("answer").innerHTML =
+      "you can see a PG-13 movie alone"
+  } else if (inputedAge >= 5) {
+    document.getElementById("answer").innerHTML =
+      "you can see a g or PG movie alone"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "your too young for most things"
+  }
+}
